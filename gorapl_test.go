2dbd12f05f52ccadef87786b09ab19006f5d3c88
fearful-symmetry <alexpkristiansen@icloud.com>
2018-11-21 22:14:51 -0600

    have the power limit read working

--- gorapl_test.go
+++ gorapl_test.go
@@ -2,6 +2,7 @@ package gorapl
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -17,4 +18,30 @@ func TestPowerLimit(t *testing.T) {
 	}
 	fmt.Printf("Got back: %#v\n", dat)
 
+}
+
+func TestParsePowerLimit(t *testing.T) {
+	var msrVal uint64 = 0xf0000140c00
+	knownParsed := RAPLPowerLimit{Limit1: PowerLimitSetting{
+		PowerLimit:      0xc00,
+		EnableLimit:     false,
+		ClampingLimit:   false,
+		TimeWindowLimit: 0xa,
+	},
+		Limit2: PowerLimitSetting{
+			PowerLimit:      0xf00,
+			EnableLimit:     false,
+			ClampingLimit:   false,
+			TimeWindowLimit: 0x0,
+		},
+		Lock: false,
+	}
+
+	parsedMsr := parsePowerLimit(msrVal)
+
+	if !reflect.DeepEqual(parsedMsr, knownParsed) {
+		t.Fatalf("struct failed: %#v", parsedMsr)
+	}
+
+	//fmt.Printf("%#v\n", parsedMsr)
 }
